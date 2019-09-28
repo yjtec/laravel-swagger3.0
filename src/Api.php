607310caf74bafcf892d0c3715073af2fa5b3c72
kidkang <723616880@qq.com>
2019-09-28 15:14:13 +0800

    add success model

--- src/Api.php
+++ src/Api.php
@@ -29,9 +29,15 @@
 /**
  *  @OA\Schema(
  *      schema="Error",
- *      required={"code", "message"},
- *      @OA\Property(property="code",type="integer",format="int32"),
- *      @OA\Property(property="message",type="string")
+ *      @OA\Property(property="errcode",type="integer",format="int32"),
+ *      @OA\Property(property="essmg",type="string")
+ *  )
+ */
+/**
+ * @OA\Schema(
+ *      schema="Success",
+ *      @OA\Property(property="errcode",type="integer",format="int32"),
+ *      @OA\Property(property="errmsg",type="string")
  *  )
  */
 /**
